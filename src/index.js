154cee555b27b920f5fedd4f5fdf882fb18fb409
Michael Plumlee <michaelplumlee95@gmail.com>
2023-04-30 17:37:03 -0700

    lab complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector("form");
+  const taskList = document.getElementById("list");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const task = form.elements.task.value;
+    const newTask = document.createElement("li");
+
+    newTask.textContent = task;
+    taskList.appendChild(newTask);
+    form.elements.task.value = "";
+  });
 });
